d3a7dd8237bc2cf97cb0f1347eee1468fc3ceb8f
wolusien <djebali.wissam@gmail.com>
2015-11-22 03:12:08 +0100

    Correction des boucle for et ajout de fonctions pour next_generation

--- Project_ocaml/projet.ml
+++ Project_ocaml/projet.ml
@@ -1,6 +1,6 @@
 type state = Val of char;;
 
-type generation = Gen of state array array;;
+type generation = state array array;;
 
 type rule = state*state*state*state*state;;
  
@@ -8,7 +8,7 @@ type automaton = rule array ;;
 
 type 'a option = None | Some of 'a;;
 
-(*Lis un fichier et le stock dans une liste*)
+(*Read file and register it on a list*)
 let read file_desc = 
   let rec add list =
        let result = 
@@ -20,17 +20,17 @@ let read file_desc =
      in add []
 ;;
 
-(*Retourne la dimension de la grille*)
+(*Return the dimension of the grid*)
 let get_dim list = int_of_string (List.hd list);;
 
-(*Recupere la ligne d'une chaine de caractères*)
-let get_string_line list string =
+(*Get the number of line of str in a list of string*)
+let get_string_line list str =
   let rec aux l s a = 
     match l with 
     |[] -> failwith("list vide")
     |h::q -> if(h=s) then (a+1)
       else aux q s (a+1)
-  in aux list string 0;;
+  in aux list str 0;;
 
 (*Recupere les lignes entre debut et fin*)
 let get_lines list debut fin =
@@ -51,28 +51,25 @@ let string_to_state_list string =
   in aux string 0 []
 ;;
 
-
-
 (*Translate string list to generation*)
 let list_to_generation list = 
-  let rec aux1 l2 t n = (*creation d'une ligne de state du tableau generation*)
-    match l2 with 
-    |[] -> t
-    |h::q -> t.(n) <- h; aux1 q t (n+1)
-  (*Récupération de la génération Zero*)   
-  in let deb_generation = (get_string_line list "GenerationZero")
+(*Récupération de la génération Zero*)   
+  let deb = (get_string_line list "GenerationZero")
   and fin = List.length list
-     in let l = get_lines list deb_generation (fin+1)(*Liste contenant la génération Zero*)
-        (*Creation du tableau tab qui contiendra la génération Zero*)
-        in let tab = (Array.init (List.length l) (fun x -> Array.init (List.length l) (fun x -> Val('A'))))
-           (*Remplissage de tab avec la génération Zero*)
-           in let aux2 l3 t1 = 
-                for i=0 to (List.length l3) 
-                do t1.(i) <- aux1 (string_to_state_list (List.nth l3 0)) (Array.init (List.length l3) (fun x -> Val('A'))) 0
-                done
-              in aux2 l tab;(*Retourne tab*) Gen(tab)
+  in let l = get_lines list deb (fin+1)
+     in let tab = (Array.init (List.length l) (fun x -> Array.init (List.length l) (fun x -> Val('A'))))
+        in let rec aux1 l2 t n = (*creation d'une ligne de state du tableau generation*)
+             match l2 with 
+             |[] -> t
+             |h::q -> t.(n) <- h; aux1 q t (n+1)
+           in let aux2 l3 t1 = (*Remplissage de tab avec la génération Zero*) 
+                for i=0 to ((List.length l3)-1) 
+                do t1.(i) <- aux1 (string_to_state_list (List.nth l3 i)) (Array.init (List.length l3) (fun x -> Val('A'))) 0
+                done;t1
+              in ((aux2 l tab):generation)(*Retourne tab (tab:generation)*)
 ;;
 
+(*Translate list of string to automaton*) 
 let list_to_rules list = 
   let rec aux1 l2 t n = 
     match l2 with 
@@ -83,13 +80,77 @@ let list_to_rules list =
   and fin = (get_string_line list "GenerationZero")
      in let l = get_lines list deb_generation fin
         in let tab = Array.init (List.length l) (fun x ->(Val('A'),Val('A'),Val('A'),Val('A'),Val('A')))
-           in (aux1 l tab 0)
+           in ((aux1 l tab 0):automaton)
 ;;
-
 
 let parse fd =
   let list = read fd
   in ((get_dim list),
 (list_to_generation list),
-((list_to_rules list):automaton))
+(list_to_rules list))
 ;;
+
+let print state = 
+  match state with 
+  |Val(a) -> print_char a
+;;
+
+let show_generation (tab:generation) = 
+  for i=0 to ((Array.length tab)-1)
+  do
+    for j=0 to ((Array.length tab)-1)
+    do print_string "|";print tab.(i).(j);print_string "|";
+    done;
+    print_string "\n";
+  done
+;;
+
+let right_cell (tab:generation) i j =
+  if((j+1)<Array.length tab.(0)) then tab.(i).(j+1)
+  else tab.(i).(0)
+;;
+
+let left_cell (tab:generation) i j = 
+  if((j-1)>=0) then tab.(i).(j-1)
+  else tab.(i).((Array.length tab.(0))-1)
+;;
+
+let north_cell (tab:generation) i j = 
+  if((i-1)>=0) then tab.(i-1).(j)
+  else tab.((Array.length tab)-1).(j)
+;;
+
+let south_cell (tab:generation) i j = 
+  if((i+1)<(Array.length tab)) then tab.(i+1).(j)
+  else tab.(0).(j)
+;;
+
+let voisin (tab:generation) l c = 
+  let cell = tab.(l).(c)(*cellule courante*)
+  in (((north_cell tab l c),(right_cell tab l c),(south_cell tab l c),(left_cell tab l c),cell):rule)
+;;
+
+(*Test if an automaton contains a rule*)
+let contains_rule rule =
+  let result = false and index = 0
+  in while (index < (Array.length tab)) 
+    do if((tab.(index))=rule) then result=true
+      else index=index+1
+    done; result
+;;
+  
+
+let next_generation (tab:generation) (rules:automaton) = 
+  let newtab = Array.init (Array.length tab) (fun x -> (Array.init (Array.length tab) (fun x -> Val('D'))))
+  in for i=0 to ((Array.length tab)-1)
+    do for j=0 to ((Array.length tab)-1)
+      do if(contains_rule rules (voisin tab i j)) then newtab.(i).(j) <- Val('A')
+        else ();
+      done;
+    done;
+  (newtab:generation)
+;;
+
+let (dim,gen,auto)= parse (open_in "testgen");;
+
+show_generation (next_generation gen auto);;
